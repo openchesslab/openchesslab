ee32637ff076d53fda9266ec55e9ee5da9ad2423
Jeroen Weelink <jeroen@jeroenweelink.com>
2026-09-22 10:20:14 +0200

    Add chess board component

---
+++ apps/web/lib/web/components/chess_board.ex
@@ -0,0 +1,48 @@
+defmodule Web.Components.ChessBoard do
+  use Phoenix.Component
+
+  alias Chess.Position
+  alias Chess.Square
+
+  attr(:position, :any, required: true)
+
+  def chess_board(assigns) do
+    assigns =
+      assign(
+        assigns,
+        :squares,
+        for(rank <- 7..0//-1, file <- 0..7) do
+          rank * 8 + file
+        end
+      )
+
+    ~H"""
+    <div id="chess-board">
+      <div
+        :for={square <- @squares}
+        id={"square-#{Square.to_algebraic(square)}"}
+        data-square={Square.to_algebraic(square)}
+      >
+        <%= if piece = Position.piece_at(@position, square) do %>
+          <span id={"piece-#{Square.to_algebraic(square)}"}>
+            {piece_symbol(piece)}
+          </span>
+        <% end %>
+      </div>
+    </div>
+    """
+  end
+
+  defp piece_symbol({:white, :king}), do: "♔"
+  defp piece_symbol({:white, :queen}), do: "♕"
+  defp piece_symbol({:white, :rook}), do: "♖"
+  defp piece_symbol({:white, :bishop}), do: "♗"
+  defp piece_symbol({:white, :knight}), do: "♘"
+  defp piece_symbol({:white, :pawn}), do: "♙"
+  defp piece_symbol({:black, :king}), do: "♚"
+  defp piece_symbol({:black, :queen}), do: "♛"
+  defp piece_symbol({:black, :rook}), do: "♜"
+  defp piece_symbol({:black, :bishop}), do: "♝"
+  defp piece_symbol({:black, :knight}), do: "♞"
+  defp piece_symbol({:black, :pawn}), do: "♟"
+end
---
+++ apps/web/test/web/components/chess_board_test.exs
@@ -0,0 +1,57 @@
+defmodule Web.Components.ChessBoardTest do
+  use ExUnit.Case, async: true
+
+  import Phoenix.LiveViewTest
+
+  alias Chess.Position
+  alias Web.Components.ChessBoard
+
+  describe "chess_board/1" do
+    test "renders all 64 squares" do
+      html =
+        render_component(&ChessBoard.chess_board/1,
+          position: Position.starting_position()
+        )
+
+      assert html =~ ~s(id="chess-board")
+
+      for square <- ~w(
+            a1 b1 c1 d1 e1 f1 g1 h1
+            a2 b2 c2 d2 e2 f2 g2 h2
+            a3 b3 c3 d3 e3 f3 g3 h3
+            a4 b4 c4 d4 e4 f4 g4 h4
+            a5 b5 c5 d5 e5 f5 g5 h5
+            a6 b6 c6 d6 e6 f6 g6 h6
+            a7 b7 c7 d7 e7 f7 g7 h7
+            a8 b8 c8 d8 e8 f8 g8 h8
+          ) do
+        assert html =~ ~s(id="square-#{square}")
+      end
+    end
+
+    test "renders pieces on their squares" do
+      html =
+        render_component(&ChessBoard.chess_board/1,
+          position: Position.starting_position()
+        )
+
+      assert html =~ ~r/id="piece-a1"[^>]*>\s*♖\s*</
+      assert html =~ ~r/id="piece-e1"[^>]*>\s*♔\s*</
+      assert html =~ ~r/id="piece-e2"[^>]*>\s*♙\s*</
+
+      assert html =~ ~r/id="piece-a8"[^>]*>\s*♜\s*</
+      assert html =~ ~r/id="piece-e8"[^>]*>\s*♚\s*</
+      assert html =~ ~r/id="piece-e7"[^>]*>\s*♟\s*</
+    end
+
+    test "renders an empty square without a piece" do
+      html =
+        render_component(&ChessBoard.chess_board/1,
+          position: Position.starting_position()
+        )
+
+      assert html =~ ~s(id="square-e4")
+      refute html =~ ~s(id="piece-e4")
+    end
+  end
+end
