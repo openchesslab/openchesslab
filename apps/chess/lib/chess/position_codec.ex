963baa24a57b560fad32917a37730777165e9ea1
Jeroen Weelink <jeroen@jeroenweelink.com>
2026-09-23 15:38:13 +0200

    Decode persisted chess positions

--- apps/chess/lib/chess/position_codec.ex
+++ apps/chess/lib/chess/position_codec.ex
@@ -5,6 +5,8 @@ defmodule Chess.PositionCodec do
 
   alias Chess.Position
 
+  @record_size 67
+
   def encode(%Position{} = position) do
     board =
       position.board
@@ -15,6 +17,157 @@ defmodule Chess.PositionCodec do
     <<board::binary, encode_side_to_move(position.side_to_move),
       encode_castling_rights(position.castling_rights), encode_en_passant(position.en_passant)>>
   end
+
+  @spec decode(binary()) ::
+          {:ok, Position.t()}
+          | {:error, :invalid_record_size}
+          | {:error, {:invalid_piece, byte()}}
+          | {:error, {:invalid_side_to_move, byte()}}
+          | {:error, {:invalid_castling_rights, byte()}}
+          | {:error, {:invalid_en_passant, byte()}}
+  def decode(encoded)
+      when is_binary(encoded) do
+    if byte_size(encoded) == @record_size do
+      <<
+        board::binary-size(64),
+        side_to_move,
+        castling_rights,
+        en_passant
+      >> = encoded
+
+      with {:ok, board} <-
+             decode_board(board),
+           {:ok, side_to_move} <-
+             decode_side_to_move(side_to_move),
+           {:ok, castling_rights} <-
+             decode_castling_rights(castling_rights),
+           {:ok, en_passant} <-
+             decode_en_passant(en_passant) do
+        {:ok,
+         Position.new(
+           board: board,
+           side_to_move: side_to_move,
+           castling_rights: castling_rights,
+           en_passant: en_passant
+         )}
+      end
+    else
+      {:error, :invalid_record_size}
+    end
+  end
+
+  defp decode_board(encoded) do
+    encoded
+    |> :binary.bin_to_list()
+    |> Enum.reduce_while(
+      {:ok, []},
+      fn value, {:ok, pieces} ->
+        case decode_piece(value) do
+          {:ok, piece} ->
+            {:cont, {:ok, [piece | pieces]}}
+
+          {:error, reason} ->
+            {:halt, {:error, reason}}
+        end
+      end
+    )
+    |> case do
+      {:ok, pieces} ->
+        {:ok,
+         pieces
+         |> Enum.reverse()
+         |> List.to_tuple()}
+
+      error ->
+        error
+    end
+  end
+
+  defp decode_piece(0), do: {:ok, nil}
+
+  defp decode_piece(1), do: {:ok, {:white, :pawn}}
+  defp decode_piece(2), do: {:ok, {:white, :knight}}
+  defp decode_piece(3), do: {:ok, {:white, :bishop}}
+  defp decode_piece(4), do: {:ok, {:white, :rook}}
+  defp decode_piece(5), do: {:ok, {:white, :queen}}
+  defp decode_piece(6), do: {:ok, {:white, :king}}
+
+  defp decode_piece(7), do: {:ok, {:black, :pawn}}
+  defp decode_piece(8), do: {:ok, {:black, :knight}}
+  defp decode_piece(9), do: {:ok, {:black, :bishop}}
+  defp decode_piece(10), do: {:ok, {:black, :rook}}
+  defp decode_piece(11), do: {:ok, {:black, :queen}}
+  defp decode_piece(12), do: {:ok, {:black, :king}}
+
+  defp decode_piece(value),
+    do: {:error, {:invalid_piece, value}}
+
+  defp decode_side_to_move(0),
+    do: {:ok, :white}
+
+  defp decode_side_to_move(1),
+    do: {:ok, :black}
+
+  defp decode_side_to_move(value),
+    do: {:error, {:invalid_side_to_move, value}}
+
+  defp decode_castling_rights(value)
+       when value in 0..15 do
+    rights =
+      []
+      |> maybe_decode_castling_right(
+        value,
+        :white_kingside,
+        0
+      )
+      |> maybe_decode_castling_right(
+        value,
+        :white_queenside,
+        1
+      )
+      |> maybe_decode_castling_right(
+        value,
+        :black_kingside,
+        2
+      )
+      |> maybe_decode_castling_right(
+        value,
+        :black_queenside,
+        3
+      )
+      |> MapSet.new()
+
+    {:ok, rights}
+  end
+
+  defp decode_castling_rights(value),
+    do: {:error, {:invalid_castling_rights, value}}
+
+  defp maybe_decode_castling_right(
+         rights,
+         value,
+         right,
+         bit
+       ) do
+    if Bitwise.band(
+         value,
+         Bitwise.bsl(1, bit)
+       ) != 0 do
+      [right | rights]
+    else
+      rights
+    end
+  end
+
+  defp decode_en_passant(255),
+    do: {:ok, nil}
+
+  defp decode_en_passant(square)
+       when square in 0..63,
+       do: {:ok, square}
+
+  defp decode_en_passant(value),
+    do: {:error, {:invalid_en_passant, value}}
 
   defp encode_piece(nil), do: 0
 
--- apps/chess/test/chess/position_codec_test.exs
+++ apps/chess/test/chess/position_codec_test.exs
@@ -69,4 +69,112 @@ defmodule Chess.PositionCodecTest do
                PositionCodec.encode(black_pawn)
     end
   end
+
+  describe "decode/1" do
+    test "round trips the starting position" do
+      position =
+        Position.starting_position()
+
+      assert position
+             |> PositionCodec.encode()
+             |> PositionCodec.decode() ==
+               {:ok, position}
+    end
+
+    test "round trips all position state" do
+      position =
+        Position.starting_position()
+        |> Map.put(:side_to_move, :black)
+        |> Map.put(
+          :castling_rights,
+          MapSet.new([
+            :white_kingside,
+            :black_queenside
+          ])
+        )
+        |> Map.put(
+          :en_passant,
+          Square.from_algebraic("e3")
+        )
+
+      assert position
+             |> PositionCodec.encode()
+             |> PositionCodec.decode() ==
+               {:ok, position}
+    end
+
+    test "rejects a record with the wrong size" do
+      assert PositionCodec.decode(<<1, 2, 3>>) ==
+               {:error, :invalid_record_size}
+    end
+
+    test "rejects an unknown piece encoding" do
+      encoded =
+        Position.new()
+        |> PositionCodec.encode()
+
+      <<_first, rest::binary>> =
+        encoded
+
+      assert PositionCodec.decode(<<99, rest::binary>>) ==
+               {:error, {:invalid_piece, 99}}
+    end
+
+    test "rejects an invalid side to move" do
+      <<
+        board::binary-size(64),
+        _side,
+        castling,
+        en_passant
+      >> =
+        Position.new()
+        |> PositionCodec.encode()
+
+      assert PositionCodec.decode(<<
+               board::binary,
+               2,
+               castling,
+               en_passant
+             >>) ==
+               {:error, {:invalid_side_to_move, 2}}
+    end
+
+    test "rejects invalid castling bits" do
+      <<
+        board::binary-size(64),
+        side,
+        _castling,
+        en_passant
+      >> =
+        Position.new()
+        |> PositionCodec.encode()
+
+      assert PositionCodec.decode(<<
+               board::binary,
+               side,
+               16,
+               en_passant
+             >>) ==
+               {:error, {:invalid_castling_rights, 16}}
+    end
+
+    test "rejects an invalid en passant square" do
+      <<
+        board::binary-size(64),
+        side,
+        castling,
+        _en_passant
+      >> =
+        Position.new()
+        |> PositionCodec.encode()
+
+      assert PositionCodec.decode(<<
+               board::binary,
+               side,
+               castling,
+               64
+             >>) ==
+               {:error, {:invalid_en_passant, 64}}
+    end
+  end
 end
